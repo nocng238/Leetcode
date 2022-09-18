d916b14cfcc09dfb85f03b77ea818cadd08e439b
nocg_ng <ngoc.nguyen@devplus.edu.vn>
2022-09-18 23:11:25 +0700

    commit at 18/9/2022

--- src/Stack/_150.java
+++ src/Stack/_150.java
@@ -5,21 +5,26 @@ import java.util.Stack;
 
 public class _150 {
     public static void main(String[] args) {
-    String tokens[] = {"4","13","5","/","+"};
-    System.out.println(evalRPN(tokens));
+        String tokens[] = { "4", "13", "5", "/", "+" };
+        System.out.println(evalRPN(tokens));
     }
-    static  public int evalRPN(String[] tokens) {
 
+    static public int evalRPN(String[] tokens) {
 
         Stack<Integer> stack = new Stack<>();
 
         for (String token : tokens) {
             switch (token) {
-                case "+" -> stack.push(stack.pop() + stack.pop());
-                case "-" -> stack.push(-(stack.pop() - stack.pop()));
-                case "*" -> stack.push(stack.pop() * stack.pop());
-                case "/" -> stack.push((int) ((1.00 / stack.pop()) * stack.pop()));
-                default ->  stack.push(Integer.parseInt(token));
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                case "-":
+                    stack.push(-(stack.pop() - stack.pop()));
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                case "/":
+                    stack.push((int) ((1.00 / stack.pop()) * stack.pop()));
+                default:
+                    stack.push(Integer.parseInt(token));
             }
         }
         return stack.pop();
